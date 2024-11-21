8e0d9378a4c6d0225646d901a940b2974f2b9a3f
Elle L. Em <iamellelem@gmail.com>
2024-11-21 16:48:52 -0500

    test 2

--- jax/_src/compiler.py
+++ jax/_src/compiler.py
@@ -190,10 +190,10 @@ def get_compile_options(
     assert device_assignment.computation_count() == num_partitions
     compile_options.device_assignment = device_assignment
 
-  if config.exec_time_optimization_effort > 0:
-    build_options.exec_time_optimization_effort = config.exec_time_optimization_effort
-  if config.memory_fitting_effort > 0:
-    build_options.memory_fitting_effort = config.memory_fitting_effort
+  if config.exec_time_optimization_effort.value > 0:
+    build_options.exec_time_optimization_effort = config.exec_time_optimization_effort.value
+  if config.memory_fitting_effort.value > 0:
+    build_options.memory_fitting_effort = config.memory_fitting_effort.value
 
   if env_options_overrides is not None:
     # Some overrides are passed directly on build_options.
